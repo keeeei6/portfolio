34b3d3984a914fff2cf9ebded72fdd99c6be13a6
keeeei6 <65399740+keeeei6@users.noreply.github.com>
2020-09-02 07:43:57 +0900

    fist

---
+++ database/migrations/2020_09_01_112522_create_categories_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('income');
+            $table->integer('rent')->nullable($value = true);
+            $table->integer('utility')->nullable($value = true);
+            $table->integer('credit')->nullable($value = true);
+            $table->integer('etc')->nullable($value = true);
+            $table->integer('month');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
