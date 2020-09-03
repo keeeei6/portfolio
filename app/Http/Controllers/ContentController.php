3d4fc672a717b43e9c4952c74cd9d69d7c929679
keeeei6 <65399740+keeeei6@users.noreply.github.com>
2020-09-04 08:58:41 +0900

    詳細表示

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -23,7 +23,7 @@ class ContentController extends Controller
     {   
         $contents = DB::table('categories')
         ->where('user_id', Auth::id())
-        ->select('month')
+        ->select('id','month')
         ->get();
         
         
@@ -61,7 +61,7 @@ class ContentController extends Controller
 
         $content->save();
 
-        return redirect('/');
+        return redirect('content');
     }
 
     /**
@@ -72,7 +72,24 @@ class ContentController extends Controller
      */
     public function show($id)
     {
-        return view('content.show');
+        $content = Category::find($id);
+        //固定出費合計
+        $cost = $content->rent + $content->utility + $content->credit + $content->etc;
+        //自由に使えるお金
+        $wants = ($content->income - $cost)*0.6;
+        //貯金
+        $saving = $content->income - $cost - $wants;
+        //グラフ用
+        $cost_proportion = $cost / $content->income;
+        $cost_proportion = round($cost_proportion, 2)*100;
+        $wants_proportion = $wants / $content->income;
+        $wants_proportion = round($wants_proportion, 2)*100;
+        $saving_proportion = $saving / $content->income;
+        $saving_proportion = round($saving_proportion, 2)*100;
+
+        return view('content.show', 
+        compact('content','cost', 'wants', 'saving','cost_proportion','wants_proportion', 'saving_proportion'));
+        
     }
 
     /**
@@ -83,7 +100,9 @@ class ContentController extends Controller
      */
     public function edit($id)
     {
-        //
+        $content = Category::find($id);
+
+        return view('content.edit', compact('content'));
     }
 
     /**
--- resources/views/content/show.blade.php
+++ resources/views/content/show.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">詳細</div>
+                <div class="card-header">{{ $content->month }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,9 +13,79 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    
+                    <form method="GET" action="{{ route('content.edit', ['id' => $content->id ]) }}">
+                    <button type="submit" class="btn btn-outline-primary">
+                        編集
+                    </button>
+                    </form>
+                    <br>
+                    <table class="table">
+                      <thead class="thead-dark">
+                        <tr>
+                          <th scope="col">カテゴリー</th>
+                          <th scope="col">小カテゴリー</th>
+                          <th scope="col">金額</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th scope="row">収入</th>
+                          <td></td>
+                          <th scope="row">¥ {{ number_format($content->income) }} </th>
+                        </tr>
 
-                    詳細表示
+                        <tr>
+                          <th scope="row">費用 <br> (NEEDS)</th>
+                          <td></td>
+                          <th scope="row">¥ {{ number_format($cost) }} </th>
+                        </tr>
+                      <thead class="thead-light">
+                        <tr>
+                          <th scope="row"></th>
+                          <th>家賃</th>
+                          <th>¥ {{ number_format($content->rent) }} </th>
+                        </tr>
+
+                        <tr>
+                          <th scope="row"></th>
+                          <th>光熱費</th>
+                          <th>¥ {{ number_format($content->utility) }} </th>
+                        </tr>
+
+                        <tr>
+                          <th scope="row"></th>
+                          <th>クレジット</th>
+                          <th>¥ {{ number_format($content->credit) }} </th>
+                        </tr>
+
+                        <tr>
+                          <th scope="row"></th>
+                          <th>その他</th>
+                          <th>¥ {{ number_format($content->etc) }} </th>
+                        </tr>
+                      </thead>
+                        <tr>
+                          <th scope="row">自由に使えるお金 <br> (WANTS)</th>
+                          <td></td>
+                          <th scope="row">¥ {{ number_format($wants) }} </th>
+                        </tr>
+
+                        <tr>
+                          <th scope="row">貯金 <br> (SAVING)</th>
+                          <td></td>
+                          <th scope="row">¥ {{ number_format($saving) }} </th>
+                        </tr>
+                      </tbody>
+                    </table>
                 </div>
+            </div>
+            <br>
+            <br>
+            <div class="progress" style="height: 50px;">
+              <div class="progress-bar" role="progressbar" style="width: <?php echo $cost_proportion;?>% " aria-valuemin="0" aria-valuemax="100">NEEDS <?php echo $cost_proportion;?>%</div>
+              <div class="progress-bar bg-success" role="progressbar" style="width: <?php echo $wants_proportion;?>% " aria-valuemin="0" aria-valuemax="100">WANTS <?php echo $wants_proportion;?>%</div>
+              <div class="progress-bar bg-info" role="progressbar" style="width: <?php echo $saving_proportion;?>%" aria-valuemin="0" aria-valuemax="100">SAVING <?php echo $saving_proportion;?>%</div>
             </div>
         </div>
     </div>
